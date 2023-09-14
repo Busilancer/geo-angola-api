5dc114f66b921c28e2af22bcbec0181794a94041
busilancer <busilancer@gmail.com>
2023-09-14 20:36:28 +0100

    update api routes

--- routes/web.php
+++ routes/web.php
@@ -17,29 +17,3 @@ use App\Http\Controllers\ProvinceController;
 Route::get('/', function () {
     return view('welcome');
 });
-
-
-// Exemplos de definição de rotas para controladores
-Route::get('/provinces', [ProvinceController::class, 'index']);
-Route::post('/provinces', [ProvinceController::class, 'store']);
-Route::get('/provinces/{id}', [ProvinceController::class, 'show']);
-Route::put('/provinces/{id}', [ProvinceController::class, 'update']);
-Route::delete('/provinces/{id}', [ProvinceController::class, 'destroy']);
-
-Route::get('/cities', [CityController::class, 'index']);
-Route::post('/cities', [CityController::class, 'store']);
-Route::get('/cities/{id}', [CityController::class, 'show']);
-Route::put('/cities/{id}', [CityController::class, 'update']);
-Route::delete('/cities/{id}', [CityController::class, 'destroy']);
-
-Route::get('/neighborhoods', [NeighborhoodController::class, 'index']);
-Route::post('/neighborhoods', [NeighborhoodController::class, 'store']);
-Route::get('/neighborhoods/{id}', [NeighborhoodController::class, 'show']);
-Route::put('/neighborhoods/{id}', [NeighborhoodController::class, 'update']);
-Route::delete('/neighborhoods/{id}', [NeighborhoodController::class, 'destroy']);
-
-Route::get('/municipalities', [MunicipalityController::class, 'index']);
-Route::post('/municipalities', [MunicipalityController::class, 'store']);
-Route::get('/municipalities/{id}', [MunicipalityController::class, 'show']);
-Route::put('/municipalities/{id}', [MunicipalityController::class, 'update']);
-Route::delete('/municipalities/{id}', [MunicipalityController::class, 'destroy']);
